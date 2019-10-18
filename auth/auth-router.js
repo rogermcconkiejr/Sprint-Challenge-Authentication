234c58ae5b60a696f2173437dc800fad54830d8e
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-10-18 09:35:28 -0600

    authenticate middleware written

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,12 +3,55 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const secrets = require('../secrets');
 
+const Users = require('../users/users-model.js');
+
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); 
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        //produce token
+        const token = generateToken(user)
+        //add token to response
+        res.status(200).json({
+          message: `Welcome ${user.username}!`, token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
+function generateToken(user) {
+  const payload ={
+    username: user.username,
+
+  };
+
+  const options ={
+    expiresIn: '1h',
+  }
+  return jwt.sign(payload, secrets.jwtSecret, options)
+}
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,24 @@
+const jwt = require('jsonwebtoken');
+const secrets = require('../secrets');
+
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token){
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken)=>{
+      if(err){
+        res.status(401).json({ you: 'shall not pass!' });
+      } else {
+        req.username = decodedToken.username; 
+        next();
+      }
+    })
+  } else {
+    res.status(400).json({ message: 'No token provided.' })
+  }
 };
